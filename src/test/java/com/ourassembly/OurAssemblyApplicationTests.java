53fddf700b2eb874dd75890b4bdbbf6bbbd2a429
ys06o <yongseongg00@gmail.com>
2026-03-20 19:17:07 +0900

    최초 푸시

---
+++ src/test/java/com/ourassembly/OurAssemblyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ourassembly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurAssemblyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
