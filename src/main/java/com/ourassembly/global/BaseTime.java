b5ab32a08e9ecef3a5a836f194cd62b0bcad4a55
ys06o <yongseongg00@gmail.com>
2026-03-23 14:38:01 +0900

    basetime,gitignore

---
+++ src/main/java/com/ourassembly/global/BaseTime.java
@@ -0,0 +1,23 @@
+package com.ourassembly.global;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter //2.상속받은 엔티티가 멤버변수를 사용
+@Setter
+@MappedSuperclass // 1]엔티티의 상속용도 클래스
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTime {
+    @CreatedDate //엔티티 생성날짜/시간 주입
+    private LocalDateTime createDate;
+
+    @LastModifiedDate //엔티티 수정날짜/시간 주입
+    private LocalDateTime updateDate;
+}
